77c5a350c9d3b7b5a86f540250b509abbee587e7
Sweta-Das <dassweta1997@gmail.com>
2024-05-09 21:20:35 +0545

    Added pydantic models

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from app.models import ChatbotInput, ChatbotOutput
+from Chatbot.chatbot import conversation
+
+app = FastAPI()
+
+@app.post("/chatbot/", response_model=ChatbotOutput)
+async def generate_response(input_text: ChatbotInput):
+    # Generate response using the input text
+    response = conversation(
+        {"question": f"{input_text}"}
+    )
+    return {"response": response["text"]}
